bf2f2e511963be0e72b4eaa2eaf775f30562b574
Eric Takahashi <erictakahashi@gmail.com>
2025-02-09 10:36:53 -0300

    feat(vite): add vite path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { AppRoutesEnum } from '~/models/enums/AppRoutesEnum'
 import { Home } from '~/modules/Home/Home'
 import AppProvider from '~/providers/AppProvider'
-import { AppRoutesEnum } from './models/enums/AppRoutesEnum'
 
 const App: React.FC = () => {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  }
-});
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
+})
