2fab074020065e7663613dc8b82476f4a4224e20
Eric Takahashi <erictakahashi@gmail.com>
2025-02-09 17:47:26 -0300

    feat(shadcn): add button component

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from "~/components/ui/button"
 
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-white shadow p-4">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-lg font-bold">brand name</h1>
+        <Button>test</Button>
       </div>
     </nav>
   )
